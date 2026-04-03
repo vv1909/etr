341b5511423bba35f2ffaffd4eefb93a7ed76d83
vv1909 <vv1909vv@gmail.com>
2026-04-04 01:29:58 +0400

    python fixed.

--- room2/code/team15_py.py
+++ room2/code/team15_py.py
@@ -6,8 +6,6 @@ def is_prime(n):
     if n <= 1 or n % 2 == 0:
         return False
     
-
-
     i = 3
     while i <= math.sqrt(n):
         if n % i == 0:
@@ -20,9 +18,10 @@ def primes(numbers):
     for n in numbers:
         if is_prime(n):
             new_list.append(n)
+
     return new_list
 
-print(primes([-2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(primes([-5, -2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 
 first1000 = range(1000)
